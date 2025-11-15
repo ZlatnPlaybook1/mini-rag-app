aed71e38d4de5c23980b9c64ee2ce172fc8bc407
Ahmed Sherif <as4349124@gmail.com>
2025-11-15 12:27:42 +0200

    set openai provider

---
+++ src/stores/LLMEnums.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class LLMEnums(Enum):
+    OPENAI = "OPENAI"
+    CHONERE = "CHONERE"
+
+class OpenAIEnums(Enum):
+    SYSTEM = "system"
+    USER = "user"
+    ASSISTANT = "assistant"
+
