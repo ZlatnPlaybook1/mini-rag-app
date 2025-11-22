e9342c1fa3a56031e859d38724ef11f7a5a6ac71
Ahmed Sherif <as4349124@gmail.com>
2025-11-22 13:57:15 +0200

    asset scheme

---
+++ src/models/db_schemas/minirag/schemes/__init__.py
@@ -0,0 +1,4 @@
+from .minirag_base import SQLAIchemyBase
+from .asset import Asset
+from .datachunk import DataChunk, RetrievedDocument
+from .project import Project
---
+++ src/models/db_schemas/minirag/schemes/asset.py
@@ -0,0 +1,32 @@
+from .minirag_base import SQLAIchemyBase
+from sqlalchemy import Column, DateTime, ForeignKey, Index
+from sqlalchemy import Integer, String, func
+from sqlalchemy.dialects.postgresql import UUID, JSONB
+from sqlalchemy.orm import relationship
+import uuid
+
+
+class Asset(SQLAIchemyBase):
+
+    __tablename__ = "assets"
+  
+
+    asset_id = Column(Integer, primary_key=True, autoincrement=True)
+    asset_uuid = Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=False)
+    
+    asset_type = Column(String, nullable=False)
+    asset_name = Column(String, nullable=False)
+    asset_size = Column(Integer, nullable=False)
+    asset_config = Column(JSONB, nullable=True)
+
+    asset_project_id = Column(Integer, ForeignKey("projects.project_id"), nullable=False)
+
+    project = relationship("Project", back_populates="assets")
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
+
+    __table_args__  = (
+        Index("idx_asset_project_id",asset_project_id),
+        Index("idx_asset_type", asset_type)
+    )
