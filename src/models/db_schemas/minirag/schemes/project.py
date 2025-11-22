344a0db2b54106685de7ab97cfb3e57abadbefd7
Ahmed Sherif <as4349124@gmail.com>
2025-11-22 13:57:07 +0200

    project scheme

---
+++ src/models/db_schemas/minirag/schemes/project.py
@@ -0,0 +1,16 @@
+from .minirag_base import SQLAIchemyBase
+from sqlalchemy import Column, DateTime
+from sqlalchemy import Integer, func
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
+class Project(SQLAIchemyBase):
+    __tablename__ = "projects"
+
+    project_id = Column(Integer, primary_key=True, autoincrement=True)
+    project_uuid = Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=False)
+    
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
+    
+    
